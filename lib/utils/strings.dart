6d594efa3544c6ff5d2f263a236be092daf9495d
MSyafei01 <80620269+MSyafei01@users.noreply.github.com>
2022-06-09 22:03:53 +0700

    Moch Syafei Bayan

---
+++ lib/utils/strings.dart
@@ -0,0 +1,91 @@
+// App name
+const String app_name = 'Attendance Ultimate';
+
+// About page
+const String about_title = 'Tentang';
+const String about_app_name = '$app_name';
+const String about_developer = 'Developer: Kelompok 1';
+const String about_url = 'https://example.com';
+const String about_desc =
+    '$app_name adalah aplikasi presensi karyawan yang menggunakan sidik jari dan presensi di dalam area yang ditentukan oleh sistem manajemen.';
+
+// Attendance page
+const String attendance_not_accurate = 'M (Tidak Akurat).';
+const String attendance_accurate = 'M (Akurat).';
+const String attendance_show_alert = 'Ya, periksa';
+const String attendance_success_ms = 'Sukses !';
+const String attendance_error_server = 'Maaf, Ada Yang Salah !';
+const String attendance_sending = 'Tunggu Sebentar...';
+const String attendance_accurate_info = 'Akurasi:';
+const String attendance_on_gps = '\nPastikan Mengaktifkan GPS Anda.';
+const String attendance_button_info = 'Klik Hadir Untuk Melakukan Pemeriksaan';
+
+// Button
+const String button_scan = 'Scan QR';
+const String button_scan_attend = 'Hadir';
+
+// Barcode camera
+const String format_barcode_wrong = 'Format QR salah.';
+const String check_in_first =
+    "Anda belum check-in, silakan check-in dulu.";
+const String already_check_in = "Anda Telah Check-In";
+const String location_not_found = "Area Tidak Ditemukan";
+const String outside_area =
+    "Anda tidak dapat hadir. Lokasi Anda saat ini berada di luar area.";
+const String select_area = "Silahkan pilih daerah terlebih dahulu.";
+const String not_support_fingerprint =
+    "Maaf, perangkat Anda tidak mendukung Sidik Jari.";
+const String check_mock = "Harap tunggu kami memeriksa Anda menggunakan GPS palsu atau tidak...";
+const String fake_gps =
+    "Kami mendeteksi Anda menggunakan GPS palsu, harap matikan GPS palsu Anda.";
+const String barcode_empty = 'QR kosong';
+const String camera_permission =
+    'Maaf, maukah Anda memberi saya izin untuk menggunakan kamera?';
+const String barcode_unknown_error = 'Error tidak diketahui';
+const String barcode_permission_cam_close =
+    'Maaf, maukah Anda memberi saya izin untuk menggunakan kamera, untuk memindai kode Qr? Sentuh di luar kotak untuk menutup munculan.';
+
+// Main Menu
+const String main_menu_title = '$app_name';
+const String main_menu_check_in = 'Check-In';
+const String main_menu_check_in_title = 'Check-In Kehadiran';
+const String main_menu_check_out = 'Check-Out';
+const String main_menu_check_out_title = 'Check-Out Kehadiran';
+const String main_menu_settings = 'API';
+const String main_menu_report = 'Laporan';
+const String main_menu_about = 'Tentang';
+
+// Report page
+const String report_name = 'NAMA';
+const String report_date = 'TANGGAL';
+const String report_time = 'WAKTU';
+const String report_type = 'TIPE';
+const String report_location = 'LOKASI';
+const String report_no_data = 'Data tidak ditemukan';
+const String report_title = 'Laporan';
+
+// Settings
+const String setting_welcome_title = 'Selamat Datang di \n$app_name!';
+const String setting_desc =
+    'Silahkan melakukan pindai Kode QR yang telah didapatkan dari Sistem Manajemen';
+
+const String setting_title = 'API';
+const String setting_info =
+    'Silakan pindai kode QR terbaru dari Sistem Manajemen';
+
+const String ok_text = 'OK';
+const String main_title = '$app_name';
+
+// Login
+const String login_checking_progress = 'Sedang Check-In...';
+const String login_failed = 'Email atau kata sandi Anda salah.';
+const String login_double_check =
+    'Sepertinya email dan kata sandi Anda telah diubah. Silakan masuk lagi.';
+const String login_title = 'Masuk';
+const String login_empty_email = 'Email diperlukan';
+const String login_label_email = 'Email';
+const String login_label_password = 'Kata Sandi';
+const String login_empty_password = 'Kata Sandi diperlukan';
+const String login_button = 'Masuk';
+const String login_wrong_role =
+    'Tidak bisa Masuk, hanya Karyawan yang dapat masuk ke aplikasi ini.';

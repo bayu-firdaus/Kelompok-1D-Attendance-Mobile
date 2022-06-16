c59393e9f1d7c28185282f4357b4f24a802ed313
bayu-firdaus <80634955+bayu-firdaus@users.noreply.github.com>
2022-06-16 21:15:03 +0700

    Update strings.dart

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -5,14 +5,14 @@ const String app_name = 'Attendance Ultimate';
 const String about_title = 'Tentang';
 const String about_app_name = '$app_name';
 const String about_developer = 'Developer: Kelompok 1';
-const String about_url = 'https://example.com';
+const String about_url = 'https:/AttendanceUltimate.com';
 const String about_desc =
     '$app_name adalah aplikasi presensi karyawan yang menggunakan sidik jari dan presensi di dalam area yang ditentukan oleh sistem manajemen.';
 
 // Attendance page
 const String attendance_not_accurate = 'M (Tidak Akurat).';
 const String attendance_accurate = 'M (Akurat).';
-const String attendance_show_alert = 'Ya, periksa';
+const String attendance_show_alert = 'Check';
 const String attendance_success_ms = 'Sukses !';
 const String attendance_error_server = 'Maaf, Ada Yang Salah !';
 const String attendance_sending = 'Tunggu Sebentar...';
@@ -25,25 +25,25 @@ const String button_scan = 'Scan QR';
 const String button_scan_attend = 'Hadir';
 
 // Barcode camera
-const String format_barcode_wrong = 'Format QR salah.';
+const String format_barcode_wrong = 'Format QR Salah.';
 const String check_in_first =
-    "Anda belum check-in, silakan check-in dulu.";
-const String already_check_in = "Anda Telah Check-In";
+    "Anda Belum Presensi Masuk, Silakan Presensi Masuk Dulu.";
+const String already_check_in = "Anda Telah Presensi Masuk";
 const String location_not_found = "Area Tidak Ditemukan";
 const String outside_area =
-    "Anda tidak dapat hadir. Lokasi Anda saat ini berada di luar area.";
-const String select_area = "Silahkan pilih daerah terlebih dahulu.";
+    "Anda Tidak Dapat Hadir. Lokasi Anda Saat Ini Berada Di Luar Area.";
+const String select_area = "Silahkan Pilih Daerah Terlebih Dahulu.";
 const String not_support_fingerprint =
-    "Maaf, perangkat Anda tidak mendukung Sidik Jari.";
-const String check_mock = "Harap tunggu kami memeriksa Anda menggunakan GPS palsu atau tidak...";
+    "Maaf, Perangkat Anda Tidak Mendukung Sidik Jari.";
+const String check_mock = "Harap Tunggu Kami Memeriksa Anda Menggunakan GPS Palsu atau Tidak...";
 const String fake_gps =
-    "Kami mendeteksi Anda menggunakan GPS palsu, harap matikan GPS palsu Anda.";
-const String barcode_empty = 'QR kosong';
+    "Kami Mendeteksi Anda Menggunakan GPS Palsu, Harap Matikan GPS Palsu Anda.";
+const String barcode_empty = 'QR Kosong';
 const String camera_permission =
-    'Maaf, maukah Anda memberi saya izin untuk menggunakan kamera?';
-const String barcode_unknown_error = 'Error tidak diketahui';
+    'Maaf, Maukah Anda Memberi Saya Izin Untuk Menggunakan Kamera?';
+const String barcode_unknown_error = 'Error Tidak Diketahui';
 const String barcode_permission_cam_close =
-    'Maaf, maukah Anda memberi saya izin untuk menggunakan kamera, untuk memindai kode Qr? Sentuh di luar kotak untuk menutup munculan.';
+    'Maaf, Maukah Anda Memberi Saya Izin Untuk Menggunakan Kamera, Untuk Memindai Kode QR? Sentuh Di Luar Kotak Untuk Menutup Munculan.';
 
 // Main Menu
 const String main_menu_title = '$app_name';
@@ -61,7 +61,7 @@ const String report_date = 'TANGGAL';
 const String report_time = 'WAKTU';
 const String report_type = 'TIPE';
 const String report_location = 'LOKASI';
-const String report_no_data = 'Data tidak ditemukan';
+const String report_no_data = 'Data Tidak Ditemukan';
 const String report_title = 'Laporan';
 
 // Settings
@@ -71,21 +71,21 @@ const String setting_desc =
 
 const String setting_title = 'API';
 const String setting_info =
-    'Silakan pindai kode QR terbaru dari Sistem Manajemen';
+    'Silakan Pindai Kode QR Terbaru dari Sistem Manajemen';
 
 const String ok_text = 'OK';
 const String main_title = '$app_name';
 
 // Login
-const String login_checking_progress = 'Sedang Check-In...';
-const String login_failed = 'Email atau kata sandi Anda salah.';
+const String login_checking_progress = 'Sedang Masuk...';
+const String login_failed = 'Email atau Kata Sandi Anda Salah.';
 const String login_double_check =
-    'Sepertinya email dan kata sandi Anda telah diubah. Silakan masuk lagi.';
+    'Sepertinya Email dan Kata Sandi Anda Telah Diubah. Silakan Masuk Lagi.';
 const String login_title = 'Masuk';
-const String login_empty_email = 'Email diperlukan';
+const String login_empty_email = 'Email Diperlukan';
 const String login_label_email = 'Email';
 const String login_label_password = 'Kata Sandi';
-const String login_empty_password = 'Kata Sandi diperlukan';
+const String login_empty_password = 'Kata Sandi Diperlukan';
 const String login_button = 'Masuk';
 const String login_wrong_role =
-    'Tidak bisa Masuk, hanya Karyawan yang dapat masuk ke aplikasi ini.';
+    'Tidak Bisa Masuk, Hanya Karyawan Yang Dapat Masuk ke Aplikasi Ini.';

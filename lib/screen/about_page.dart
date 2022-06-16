82f6ae6abaad1ebea3a80b4ab3ba4f5fb7ec53ea
bayu-firdaus <80634955+bayu-firdaus@users.noreply.github.com>
2022-06-16 21:11:20 +0700

    Update about_page.dart

--- lib/screen/about_page.dart
+++ lib/screen/about_page.dart
@@ -1,15 +1,16 @@
-import '../utils/strings.dart';
+import 'package:attendancewithfingerprint/utils/strings.dart';
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff242559),
       appBar: AppBar(
-        title: Text(about_title),
+        title: Text(about_title)
       ),
       body: Container(
-        margin: EdgeInsets.all(10.0),
+        margin: EdgeInsets.all(30.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
@@ -17,32 +18,32 @@ class AboutPage extends StatelessWidget {
               image: AssetImage('images/logo.png'),
             ),
             SizedBox(
-              height: 20.0,
+              height: 10.0,
             ),
             Center(
               child: Text(
                 about_app_name,
                 textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                style: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),
               ),
             ),
             SizedBox(
-              height: 20.0,
+              height: 30.0,
             ),
             Text(
               about_developer,
-              style: TextStyle(fontSize: 13.0, color: Colors.grey),
+              style: TextStyle(fontSize: 15.0, color: Colors.grey),
             ),
             Text(
               about_url,
               style: TextStyle(fontSize: 13.0, color: Colors.grey),
             ),
             SizedBox(
-              height: 20.0,
+              height: 30.0,
             ),
             Text(
               about_desc,
-              style: TextStyle(fontSize: 15.0, color: Colors.grey),
+              style: TextStyle(fontSize: 18.0, color: Colors.grey),
               textAlign: TextAlign.center,
             ),
           ],
